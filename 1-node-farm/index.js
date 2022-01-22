720fef56643d296a4b00cb788ad99ff4c172ae76
Dipto-32 <dipto.8081@gmail.com>
2022-01-22 01:36:26 -0800

    module replaceTemplate added

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const replaceTemplate= require('./starter/modules/replaceTemplate');
+
+
+
 /////////////////////////////
 //Files
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
@@ -11,22 +15,8 @@ const dataObject = JSON.parse(data);
 
 
 ///////////////////////////
-//server
+////////server////////////
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%PRODUCTNUTRIENTSNAME%}/g, product.nutrients);
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/, 'not-organic');
-    }
-    return output;
-}
 
 const server = http.createServer((req, res) => {
 
